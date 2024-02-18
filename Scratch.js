af51fb1c8b15e8acd856320063d42cf999869e28
Graham Cohick <78755156+GC-171@users.noreply.github.com>
2024-02-18 18:39:10 -0500

    Create Scratch.js

---
+++ Scratch.js
@@ -0,0 +1,3 @@
+var Count = document.querySelector("div")._reactRootContainer._internalRoot.current.child.stateNode.store.getState().scratchGui.vm.runtime.targets[0].lookupVariableByNameAndType("Count","").value;
+
+console.log(Count);
